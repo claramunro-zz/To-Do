a243c8b47d8fef6dd622d2c58db266ae9b3352f6
Guest User <Guest@epicodus-25.local>
2019-04-01 13:03:46 -0700

    angular practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Task } from './models/task.model';
 @Component({
-  selector: 'app-root',
+  selector: 'crazy-app-selector',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDay();
+  year: number = this.currentTime.getFullYear();
+  firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
 }
+
