055911ffefbfdeab022be328bd2046b72eb7d833
Student <example@example.com>
2019-04-02 14:37:47 -0700

    working firebase app, deployed, pipes & more components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,25 @@ export class AppComponent {
   currentFocus: string = 'Angular Homework';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
-  day: number = this.currentTime.getDay();
+  day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
+  selectedTask = null;
+
+  masterTaskList: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask = null;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
   }
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
+
   finishedEditing() {
     this.selectedTask = null;
   }
-}
 
+  addTask(newTask: Task) {
+    this.masterTaskList.push(newTask);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule }  from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { CompletenessPipe } from './completeness.pipe';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent,
+    CompletenessPipe
   ],
   imports: [
     BrowserModule,
